7581b4491328393e6e6866be7490c231f1759ae5
turtletown73 <108070070+turtletown73@users.noreply.github.com>
2024-01-24 10:36:37 -0500

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,5 +1,7 @@
 var script = document.getElementById("extraScript");
 var iframe = document.getElementById("frame");
+var window = iframe.contentWindow;
+var gameInstance = window.unityInstance;
 
 alert("Launched With Extras!");
 if (script.armor == true) {
