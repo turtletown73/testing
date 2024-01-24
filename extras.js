127548031baa291f60077a04ad11d49d2fabb95e
turtletown73 <108070070+turtletown73@users.noreply.github.com>
2024-01-24 10:43:41 -0500

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,6 +1,6 @@
 var script = document.getElementById("extraScript");
 var iframe = document.getElementById("frame");
-var window = iframe.contentWindow;
+var windowThing = iframe.contentWindow;
 var gameInstance = window.unityInstance;
 
 alert("Launched With Extras!");
@@ -8,7 +8,7 @@ if (script.armor == true) {
   alert("With Infinite Armor!");
 }
 
-document.onkeypress = function(e) {
+windowThing.document.onkeypress = function(e) {
   if (e.keyCode == 220) {
     alert(JSON.stringify(gameInstance));
   }
