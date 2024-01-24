0ddb3ff0ed9b52b9425f8022ca9cb2b69005c47d
turtletown73 <108070070+turtletown73@users.noreply.github.com>
2024-01-24 14:35:46 -0500

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,12 +1,5 @@
 var script = document.getElementById("extraScript");
 var iframe = script.parentElement;
-var windowThing = null;
-var gameInstance = null;
-
-iframe.onload = function () {
-  windowThing = iframe.contentWindow;
-  gameInstance = windowThing.unityInstance;
-}
 
 alert("Launched With Extras!");
 if (script.armor == true) {
@@ -15,6 +8,6 @@ if (script.armor == true) {
 
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == 13) {
-    alert(windowThing);
+    alert(script.parentElement);
   }
 }, false);
